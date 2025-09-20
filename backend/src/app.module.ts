5d3f630381a0843f356e15e2b313321c5d184e88
Samy Hamlat <samy.hamlat01@gmail.com>
2025-09-20 14:57:14 +0200

    Feat(ECO-001)
    Added configuration for Typeorm for the postgreSQL db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: configService.get<string>('NODE_ENV') === 'development',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
